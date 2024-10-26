c9af0c2e19cc8cff6e21ddfdc0de60c72642a466
Luiz henrique <luizpq33@gmail.com>
2024-10-26 13:57:47 -0400

    Melhorando a segurança dos tokens

--- core/settings.py
+++ core/settings.py
@@ -153,7 +153,9 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=365),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 # Mercado Pago
