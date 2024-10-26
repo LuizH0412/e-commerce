d4564b193c134f9463fe7c6556fedff55bcb6328
Luiz henrique <luizpq33@gmail.com>
2024-10-26 00:32:59 -0400

    retornando o banco de dados para sqllite para realizar o deploy

--- core/settings.py
+++ core/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
